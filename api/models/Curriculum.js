1c48cace1d81941dde852b2ffc766efaa77f5990
GABRIELA SCHIRMER <gabriela_schirmer@estudante.sesisenai.org.br>
2024-11-28 20:42:43 -0300

    acertando formacao e experiencias

--- api/models/Curriculum.js
+++ api/models/Curriculum.js
@@ -5,16 +5,20 @@ const curriculumSchema = new mongoose.Schema({
   email: { type: String, required: true },
   telefone: { type: String, required: true },
   endereco: { type: String, required: true },
-  formacao: {
-    curso: { type: String, required: true },
-    instituicao: { type: String, required: true },
-    anoConclusao: { type: Number, required: true }
-  },
-  experiencia: {
-    empresa: { type: String, required: true },
-    cargo: { type: String, required: true },
-    periodo: { type: String, required: true }
-  },
+  formacoes: [
+    {
+      curso: { type: String, required: true },
+      instituicao: { type: String, required: true },
+      anoConclusao: { type: Number, required: true },
+    },
+  ],
+  experiencias: [
+    {
+      empresa: { type: String, required: true },
+      cargo: { type: String, required: true },
+      periodo: { type: String, required: true },
+    },
+  ],
   habilidades: { type: [String], required: true },
   idiomas: { type: [String], required: true },
   objetivo: { type: String, required: true }
